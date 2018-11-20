086261cc334db2b3a3da457591c90a4d228258c6
Allison Medina da Silva <38405889+allisonmdn@users.noreply.github.com>
2018-11-20 04:33:55 -0200

    User-reg+login.

--- Gameprojeto/Game/User.cpp
+++ Gameprojeto/Game/User.cpp
@@ -4,44 +4,102 @@
 
 User::User()
 {
+		
 }
 
 
 User::~User()
 {
 }
+void User::Login()
+{
+	us.open("Username.bin", std::ios::in | std::ios::binary);  //Open.
+	pass.open("Password.bin", std::ios::in | std::ios::binary);
+
+	while (!us.eof() && !pass.eof())
+	{
+		if (us.is_open())
+		{
+			us >> userName;
+			uStack.push_back(userName);
+			uStack.size();
+		}
+		if (pass.is_open())
+		{
+			pass >> passWord;
+			pStack.push_back(passWord);
+			pStack.size();
+		}
+	}
+
+	std::cout << "Enter your username and password.\n";
+	std::cout << "Username: ";
+	std::cin >> uNameLog;
+
+	itU = std::find(uStack.begin(), uStack.end(), uNameLog);
+	
+	if (itU != uStack.end())
+	{
+		std::cout << "Password: ";
+		std::cin >> passwLog;
+
+		itP = std::find(pStack.begin(), pStack.end(), passwLog);
+		
+		if (itP != pStack.end())
+		{	 			
+			std::cout << "Password correct!\n";			
+		}
+		else
+		{
+			std::cout << "Password incorrect!\n";
+		}
+	}
+	else
+	{
+		std::cout << "Username incorrect! \n";
+	}
+	us.close();	  //Close.
+	pass.close();
+
+}
 
 void User::Register()
 {
-	InOrOut.open("Register.bin", std::ios::out | std::ios::binary);	  //Open
+	us.open("Username.bin", std::ios::out | std::ios::binary);	  //Open
+	pass.open("Password.bin", std::ios::out | std::ios::binary);
+		
 
 	do {													  //Username
-		std::cout << "Input your username: " << std::endl;
-		std::cin >> Username;
+		std::cout << "Register your username: " << std::endl;
+		std::cin >> userName;
 		std::cout << "\n";
+		
 				
-		if (Username.length() >= 6 && Username.length() <= 12)
+		if (userName.length() >= 6 && userName.length() <= 12)
 		{
-			InOrOut << Username;
+			us << userName;
 		}
 		else
 		{
 			system("cls");
 		}
-	} while (Username.length() < 6 || Username.length() > 12);
+	} while (userName.length() < 6 || userName.length() > 12);
 	do {													   //Password
-		std::cout << "Input your password: " << std::endl;
-		std::cin >> Password;
+		std::cout << "Register your password: " << std::endl;
+		std::cin >> passWord;
 		std::cout << "\n";
-		if (Password.length() >= 6 && Password.length() <= 12)
+		std::cout << "Retype your password: " << std::endl;
+		std::cin >> passWordC;
+		if (passWordC == passWord && passWord.length() >= 6 && passWord.length() <= 12)
 		{  			
-			InOrOut << "\n" << Password << "\n";
+			pass << "\n" << passWord << "\n";
 		}
 		else
 		{
 			system("cls");					  
 		}
-	} while (Password.length() < 6 || Password.length() > 12);
+	} while (passWordC != passWord || passWord.length() < 6 || passWord.length() > 12);
 	
-	InOrOut.close();   //Close
+	us.close();   //Close
+	pass.close();
 }
--- Gameprojeto/Game/User.h
+++ Gameprojeto/Game/User.h
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+
+
 #pragma once
 class User
 {
@@ -8,10 +12,15 @@ public:
 	User();
 	~User();
 	void Register();
+	void Login();
 	
 protected:
-	std::string Username, Password;		
-	std::fstream InOrOut;
+	std::string userName, passWord, userNameC, passWordC;
+	std::vector <std::string> uStack, pStack, account;//Account[0] == uStack[0] and pStack[0]; 	
+	std::string uNameLog, passwLog;
+	std::fstream us; std::fstream pass;
+	bool acessGranted;
+	std::vector <std::string>::iterator itU, itP;
 	
 };
 
