723a3f6eae95b48a8714aeddbba0809e8b9159ce
AndersonPaim <42255485+AndersonPaim@users.noreply.github.com>
2018-12-01 23:12:15 -0200

    Personagem com tiro

---
+++ Personagem(com tiro).h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "libUnicornio.h"
+#include <time.h>
+#include "Resources_Sound.h"
+
+
+#pragma once
+class Personagem : public ObjetoTileMap
+{
+public:
+	Personagem();
+	~Personagem();
+	void setSpriteSheet(std::string sSheet);
+	virtual void draw();
+	virtual void update();
+	virtual void attack();
+	virtual void walk();
+	virtual void shot();
+	void setSpeed(float speed_);
+	float getSpeed();
+	Texto Texto_, text;
+	std::string TextoTeste = "Trabalho Grau B"; //Optional
+
+protected:
+	Vetor2D dir, dir2;
+	Sprite spr, spr2, tiro;
+	float speed;
+	//int width, height; //Screen coordinates
+	int respawn;
+	Resources_Sound Sounds;
+	Resources_Sound Theme;
+	int direcao, pontuacao, velocidade = 0;
+	bool vivo = false;
+};
